b4262e170da41943bce5cef5cc711879989926fd
waghvishalg <waghvishalg@gmail.com>
2023-08-01 17:39:33 +0530

    Initial Commit

---
+++ VishalCard/VishalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VishalCardApp.swift
+//  VishalCard
+//
+//  Created by Vishal Wagh on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct VishalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
